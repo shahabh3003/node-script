e35160ddf659506dfea4d67f1fd9b81f4e48877b
Shahab Hashmi <shahabh@vmware.com>
2022-02-08 18:45:59 +0530

    fixed bug

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const path = require("path");
 const loc = "../avi-dev/python/bin/portal/static/src"
 //const loc = './src';
 
+// iterates through the projects and gives the file path of each file in arrayOfFiles(array)
 const getAllFiles = (dirPath, arrayOfFiles) => {
   //console.log("dirPath");
   files = fs.readdirSync(dirPath);
@@ -15,10 +16,11 @@ const getAllFiles = (dirPath, arrayOfFiles) => {
       arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
     }
   })
-  //fs.close(dirPath); 
+   
   return arrayOfFiles;
 }
 
+// reads the data of each location and writes to common.l10n.ts, filters out the unnecessary part
 const getData = (fileDir, writeFile) => {
   fs.readFile(fileDir, 'utf8' , (err, data) => {
     if (err) {
@@ -40,13 +42,11 @@ const getData = (fileDir, writeFile) => {
           return;
       }
     });
-    //fs.close(writeFile);
   });
-  //fs.close(fileDir);
+  
 }
 
-const arrayOfFiles = getAllFiles(loc,[]);
-
+// iterates through arrayOfFiles and recursively calls getData function
 const appendData = () => {
   for(let i=0;i<arrayOfFiles.length;i++){
     const regex = ".l10n.";
@@ -59,52 +59,59 @@ const appendData = () => {
   console.log("Successfull");
 }
 
-appendData();
-
-console.log("Filter successful");
-
+// writes the word frequency to result.ts, called recursively by wordFreq
 const appendObjData = (data) => {
   fs.appendFile('./result.ts', data, err => {
     if(err){
-        console.error(err);
-        return;
+      console.error(err);
+      return;
     }
   });
   console.log("Data Appended");
-  //fs.close('./result.ts');
+  
 }
 
+// creates the word frequency and writes to result.ts recursively
 const wordFreq = (str) =>{
+  console.log("wordFreq", str);
   let words = str.split(' ');
   let found = {};
 
   words.map(( word ) => {
     if ( word.length >= 1 ) {
       if ( found[word] ) {
-          found[word]++;
+        found[word]++;
       }else{
-          found[word] = 1;
-        }
+        found[word] = 1;
       }
-    });
-    for(let i in found){
-      let val = `${i} : ${found[i]} \n`;
-      console.log(typeof(val));
-      appendObjData(val)
     }
-    return found;
+  });
+  console.log("wordFreq", found);
+  for(let i in found){
+    let val = `${i} : ${found[i]} \n`;
+    console.log(typeof(val));
+    appendObjData(val)
+  }
+  return found;
 }
 
+// reads common.l10n.ts and creates the word frequency
 const readCommonFile = (fileDir) => {
+  console.log("reading common file");
   fs.readFile(fileDir, 'utf8' , (err, data) => {
     if (err) {
       console.error(err)
       return
     }
+
+    console.log("reading common file", data);
     wordFreq(data);
   });
-  //fs.close(fileDir);
+  
 }
 
+const arrayOfFiles = getAllFiles(loc,[]);
+appendData();
+console.log("Filter successful");
 readCommonFile('./common.l10n.ts');
 console.log("end!!!");  
